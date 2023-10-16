8b33502ac917e16cfc89119c2fb13e924e062250
tacostrophe <tacokong@yandex.ru>
2023-10-16 18:02:44 +0300

    adding dotenv and db sync

--- user/app.js
+++ user/app.js
@@ -3,14 +3,27 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const dotenv = require('dotenv');
 
 const usersRouter = require('./routes/user.routes');
+const db = require('./models');
+
+dotenv.config();
 
 const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+// syncing db
+db.sequelize.sync()
+  .then(() => {
+    console.log('Synced db.');
+  })
+  .catch((err) => {
+    console.log(`Failed to sync db: ${err.message}`);
+  });
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,12 +34,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};

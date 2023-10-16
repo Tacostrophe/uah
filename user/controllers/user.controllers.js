b66de8cf409ce4452ea7c65d8958527ef144f878
tacostrophe <tacokong@yandex.ru>
2023-10-16 18:03:23 +0300

    adding logic to controllers

--- user/controllers/user.controllers.js
+++ user/controllers/user.controllers.js
@@ -1,13 +1,21 @@
 const asyncHandler = require('express-async-handler');
 
-exports.list = asyncHandler(async (req, res) => {
-  res.send('list of all users');
-});
+const db = require('../models');
+const createRecord = require('../utils/createRecord');
 
 exports.create = asyncHandler(async (req, res) => {
-  res.send('create user');
+  const user = { id: 1, name: 'new user' };
+  await createRecord('create', user);
+  res.send(user);
 });
 
 exports.update = asyncHandler(async (req, res) => {
-  res.send(`updating user ${req.params.id}`);
+  const user = { id: req.params.id, name: 'existing user' };
+  await createRecord('update', user);
+  res.send(user);
+});
+
+exports.list = asyncHandler(async (req, res) => {
+  const users = await db.users.findAll();
+  res.send(users);
 });

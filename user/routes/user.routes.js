c346922bcd3d240ada081f30b2e455f88bbcf368
tacostrophe <tacokong@yandex.ru>
2023-10-18 08:50:34 +0300

    added validation

--- user/routes/user.routes.js
+++ user/routes/user.routes.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const { checkExact } = require('express-validator');
+
 const users = require('../controllers/user.controllers');
+const validate = require('../middlewares/validation');
+const {
+  newUserValidationRules,
+  existingUserValidationRules,
+} = require('../middlewares/validation/userValidationRules');
+const { idValidationRules } = require('../middlewares/validation/idValidationRules');
 
 const router = express.Router();
 
-router.post('/', users.create);
-router.put('/:id', users.update);
+router.post(
+  '/',
+  validate(newUserValidationRules),
+  users.create,
+);
+router.patch(
+  '/:id',
+  validate(
+    idValidationRules,
+    existingUserValidationRules,
+  ),
+  users.update,
+);
 router.get('/', users.list);
 
 module.exports = router;

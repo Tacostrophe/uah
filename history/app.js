3983fd1a39723c1782d9bb725e79a7509a4bfdac
tacostrophe <tacokong@yandex.ru>
2023-11-08 21:34:28 +0300

    syncing db

--- history/app.js
+++ history/app.js
@@ -1,21 +1,31 @@
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const dotenv = require('dotenv');
 
+const db = require('./models');
 const recordsRoutes = require('./routes/record.routes');
 
 dotenv.config();
 
 const app = express();
 
+// syncing db
+db.sequelize.sync()
+  .then(() => {
+    console.log('Synced db.');
+  })
+  .catch((err) => {
+    console.log(`Failed to sync db: ${err.message}`);
+  });
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+// routers
 app.use('/records', recordsRoutes);
 
 module.exports = app;

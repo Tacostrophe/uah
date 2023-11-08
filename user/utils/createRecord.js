87339ac1f1bd6ec6b21f3577f42622111331f8ab
tacostrophe <tacokong@yandex.ru>
2023-11-08 21:44:16 +0300

    realisation of createRecord finally

--- user/utils/createRecord.js
+++ user/utils/createRecord.js
@@ -1,5 +1,22 @@
-async function createRecord(actionType, user) {
-  console.log(`creating record: ${actionType} user ${user.id}`);
+async function createRecord(type, userId, date) {
+  fetch('http://localhost:8081/records', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      userId,
+      type,
+      date,
+    }),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(`Record created:\n ${data}`);
+    })
+    .catch((err) => {
+      console.log(`Failed to create record: ${err}`);
+    });
 }
 
 module.exports = createRecord;

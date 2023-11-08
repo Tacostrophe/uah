093de9ef7a74eecf6da34795110868ec3812feb1
tacostrophe <tacokong@yandex.ru>
2023-11-08 21:43:07 +0300

    deleting error-handling and static

--- user/app.js
+++ user/app.js
@@ -1,6 +1,4 @@
-const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const dotenv = require('dotenv');
@@ -25,25 +23,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // routers
 app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
-});
-
-// error handler
-app.use((err, req, res) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  console.log('!!! were here !!!');
-  console.log(`${req.app.get('env')}`);
-
-  res.status(err.status || 500).send('error');
-});
 
 module.exports = app;
